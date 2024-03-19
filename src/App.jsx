50af33c1cca104a4d8f1fe3af42b60a0d8a9873e
unknown <linda.frischknecht@bluewin.ch>
2024-03-19 14:47:28 +0100

    team-coding session done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,48 @@
-// Import required  hooks
+import { useEffect, useState } from "react";
 import { DogFact } from "./components/DogFact";
+import Lottie from "react-lottie";
+import animation from "./Animation.json";
 
 export const App = () => {
   // Hint: Initialize state for storing the dog fact
+  const [dogFact, setDogFact] = useState();
+
   // Hint: Define the API endpoint
+  const URL = "https://dogapi.dog/api/v2/facts";
+
   // Hint: Create a function to fetch the dog fact
+  const handleDogFact = () => {
+    fetch(URL)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log(json);
+        setDogFact(json);
+      });
+  };
+
   // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
+  useEffect(() => {
+    handleDogFact();
+  }, []);
 
   return (
     <div className="App">
-      <DogFact />
+      {dogFact ? (
+        <DogFact dogFact={dogFact} />
+      ) : (
+        <div>
+          <Lottie
+            options={{
+              loop: true,
+              autoplay: true,
+              animationData: animation,
+            }}
+            height={400}
+            width={400}
+          />
+          <p>Loading...</p>
+        </div>
+      )}
     </div>
   );
 };
--- src/components/DogFact.jsx
+++ src/components/DogFact.jsx
@@ -1,9 +1,12 @@
 // DogFact Component
-export const DogFact = () => {
+export const DogFact = ({ dogFact }) => {
   // Hint: Extract the 'body' from 'attributes' of the 'fact' prop
+  console.log(dogFact);
+  const cleanFact = dogFact.data[0].attributes.body;
+  console.log(cleanFact);
 
   // Hint: Render the 'cleanFact' if available, otherwise show a loading message
-  return <div>dog fact here.</div>;
+  return <div className="fact">{cleanFact}</div>;
 };
 
 // Hint: To use this component, import it in your main App component and pass the 'fact' prop to it.
